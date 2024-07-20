5f29894a54d10b81b3fc26172039e3e966c78031
Shaked Yosef Sofer <shakedsof@gmail.com>
2024-07-21 02:32:43 +0300

    change title

--- src/components/Header1.js
+++ src/components/Header1.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { BsFillHouseDoorFill } from "react-icons/bs";
 
-
 export default function Header1() {
   return (
-    <header className='container-fluid bg-warning'>
-      <div className='container p-2'>
+    <header className='container-fluid bg-warning'>      
+      {/* <img src='/Stoc.png' alt='Stoc' /> */}
+      <div className='container p-2'>        
         <div className='row align-items-center'>
           <div className='logo col-auto'>
-            <h2>Noon</h2>
+            <h2>Stoc</h2>
           </div>
           <nav className='col-auto'>
             <ul>
